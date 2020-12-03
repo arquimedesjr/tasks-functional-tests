381d1a78f0191a9709f6d5a98ed22111d62ecb5d
Arquimedes Lima de Queiroz Junior <33572293+arquimedesjr@users.noreply.github.com>
2020-12-03 20:21:46 -0300

    Update TasksTest.java

--- src/test/java/br/ce/limaarq/tasks/funcional/TasksTest.java
+++ src/test/java/br/ce/limaarq/tasks/funcional/TasksTest.java
@@ -18,7 +18,7 @@ public class TasksTest {
 
     public WebDriver acessarAplicacao() throws MalformedURLException {
         WebDriver driver = new ChromeDriver();
-        DesiredCapabilities cap = DesiredCapabilities.chrome();
+      //  DesiredCapabilities cap = DesiredCapabilities.chrome();
      //   WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.14:4444/wd/hub"), cap);
         driver.navigate().to("http://192.168.0.14:8001/tasks");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
